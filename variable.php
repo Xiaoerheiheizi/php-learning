7e1cab46daff8f3c315a70bb84bd95b052a42001
Xiaoerheiheizi <463726190@qq.com>
2019-07-30 13:55:23 +0800

    task # PHP 静态变量

--- variable.php
+++ variable.php
@@ -14,5 +14,23 @@ function sayHello($hello)
 	echo $hello;
 }
 
+// 静态变量仅在局部函数域中存在，但当程序执行离开此作用域时，其值并不丢失，否则会丢失
+function test()
+{
+	static $static = 0;
+	echo $static . PHP_EOL;
+	$static++;
+}
+
+test();
+test();
+
 sayHi();
 sayHello($hello);
+
+$a = 'b';
+$b = 'c';
+$c = 'd';
+
+$$$$a = 'bcd';
+var_dump($d);
